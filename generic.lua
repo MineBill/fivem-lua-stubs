e6c197f102ed869051a13a78551ec2be55db167b
MineBill <terrestrialstudio@gmail.com>
2021-08-29 20:16:50 +0300

    Update

--- generic.lua
+++ generic.lua
@@ -1,4 +1,8 @@
 Citizen = {}
+json = {
+	encode = function(table) end
+	decode = function(jsonString) end
+}
 
 function fx_version(version) end
 function game(name) end
@@ -8,6 +12,11 @@ function author(name) end
 function description(description) end
 function version(version) end
 function dependencies(deps) end
+function dependency(dep) end
+function export(name) end
+
+function files(files) end
+function ui_page(page) end
 
 function client_script(version) end
 function client_scripts(version) end
@@ -21,3 +30,6 @@ function server_scripts(version) end
 function vector2(x, y) end
 function vector3(x, y, z) end
 function vector4(x, y, z, w) end
+
+function loadscreen(page) end
+function loadscreen_manual_shutdown(option) end
